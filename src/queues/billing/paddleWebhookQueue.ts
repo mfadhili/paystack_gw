a9d8699393cda012eb88f624e043620497227036
mfadhili <mfadhili@github.com>
2025-06-06 17:17:25 +0300

    fix build dependene

--- src/queues/billing/paddleWebhookQueue.ts
+++
@@ -1,6 +0,0 @@
-import { Queue } from "bullmq";
-import {connection} from "../../utils/queueUtils/connection";
-
-export const paddleWebhookQueue = new Queue("paddle-webhook-queue", {
-    connection,
-});
--- src/queues/initializeWorkers.ts
+++ src/queues/initializeWorkers.ts
@@ -1,37 +1,12 @@
 import { Worker, Job } from "bullmq";
 import { connection } from "../utils/queueUtils/connection";
-import { processPaddleWebhookEventService } from "../apis/services/billingServices/paddleBillingServices/webhookServices/processPaddleWebhookEventService";
 import {
     processPaystackWebhookEventService
 } from "../apis/services/billingServices/paystackBillingServices/webhookServices/processPaystackWebhookEventService";
 
-let paddleWebhookWorker: Worker | null = null;
 let paystackWebhookWorker: Worker | null = null;
 
 export const initializeWorkers = () => {
-    if (!paddleWebhookWorker) {
-        console.log("🚀 Initializing Paddle Webhook Worker...");
-
-        paddleWebhookWorker = new Worker(
-            "paddle-webhook-queue",
-            async (job:Job) => {
-                console.log(`➡️ Processing Paddle Webhook Event: ${job.id}`);
-                const { eventId } = job.data;
-                return await processPaddleWebhookEventService(eventId);
-            },
-            { connection }
-        );
-
-        paddleWebhookWorker.on("completed", (job) =>
-            console.log(`✅ Paddle Webhook Job ${job.id} completed!`)
-        );
-
-        // @ts-ignore
-        paddleWebhookWorker.on("failed", (job, err) =>
-            //@ts-ignore
-            console.error(`❌ Paddle Webhook Job ${job.id} failed: ${err.message}`)
-        );
-    }
 
     if (!paystackWebhookWorker) {
         console.log("🚀 Initializing Paystack Webhook Worker...");
@@ -46,14 +21,14 @@ export const initializeWorkers = () => {
             { connection }
         );
 
-        paddleWebhookWorker.on("completed", (job) =>
-            console.log(`✅ Paddle Webhook Job ${job.id} completed!`)
+        paystackWebhookWorker.on("completed", (job) =>
+            console.log(`✅ Paystack Webhook Job ${job.id} completed!`)
         );
 
         // @ts-ignore
-        paddleWebhookWorker.on("failed", (job, err) =>
+        paystackWebhookWorker.on("failed", (job, err) =>
             //@ts-ignore
-            console.error(`❌ Paddle Webhook Job ${job.id} failed: ${err.message}`)
+            console.error(`❌ Paystack Webhook Job ${job.id} failed: ${err.message}`)
         );
     }
 };
