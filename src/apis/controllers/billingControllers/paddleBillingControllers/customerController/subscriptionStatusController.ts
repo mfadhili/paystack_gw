28f70555ee0099de4cf9a497ba2356f4a9ba31a3
mfadhili <mfadhili@github.com>
2025-06-06 17:07:01 +0300

    fix build dependene

--- src/apis/controllers/billingControllers/paddleBillingControllers/customerController/subscriptionStatusController.ts
+++ src/apis/controllers/billingControllers/paddleBillingControllers/customerController/subscriptionStatusController.ts
@@ -3,10 +3,10 @@ import { Request, Response } from "express";
 import {
     getSubscriptionStatusService
 } from "../../../../services/billingServices/paddleBillingServices/customerServices/getSubscriptionStatusService";
-import {AuthenticatedRequest} from "../../../../middlewares/authMiddleware/authenticateUser";
+// import {AuthenticatedRequest} from "../../../../middlewares/authMiddleware/authenticateUser";
 
-export const getSubscriptionStatusController = async (req: AuthenticatedRequest, res: Response) => {
-    try {
+export const getSubscriptionStatusController = async (req: Request, res: Response) => {
+/*    try {
         const user = req.user; // From authenticateUser middleware
         const result = await getSubscriptionStatusService(user.account);
 
@@ -18,5 +18,5 @@ export const getSubscriptionStatusController = async (req: AuthenticatedRequest,
         return res.status(404).json({
             message: (error as Error).message || "Subscription not found",
         });
-    }
+    }*/
 };
