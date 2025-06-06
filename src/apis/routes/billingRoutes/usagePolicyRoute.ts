d916f458fb3db26efe2ca0581ca049a35c5d6c31
mfadhili <mfadhili@github.com>
2025-06-06 17:08:22 +0300

    fix build dependene

--- src/apis/routes/billingRoutes/usagePolicyRoute.ts
+++ src/apis/routes/billingRoutes/usagePolicyRoute.ts
@@ -8,12 +8,12 @@ import {
     getAllUsagePoliciesController
 } from "../../controllers/billingControllers/usagePolicyControllers/getUsagePolicyController";
 import {getUsageController} from "../../controllers/billingControllers/usagePolicyControllers/getUsageController";
-import {authenticateUser} from "../../middlewares/authMiddleware/authenticateUser";
+// import {authenticateUser} from "../../middlewares/authMiddleware/authenticateUser";
 
 const router = express.Router();
 
-router.post("/usage-policy", createOrUpdateUsagePolicyController);
-router.get("/get-usage-policies", getAllUsagePoliciesController); // 🔥 NEW
-router.get("/get-subscription-usage", authenticateUser, getUsageController);
+// router.post("/usage-policy", createOrUpdateUsagePolicyController);
+// router.get("/get-usage-policies", getAllUsagePoliciesController); // 🔥 NEW
+// router.get("/get-subscription-usage", authenticateUser, getUsageController);
 
 export default router;
