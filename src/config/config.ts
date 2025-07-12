e2d8df08502441e221d5758acadbd98e1e8a03aa
mfadhili <mfadhili@github.com>
2025-07-12 17:54:21 +0300

    paystack test checker

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,18 +6,14 @@ config();
 
 export const MONGODB_URI = process.env.MONGO_STRING;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
-export const NODE_ENV2 = 'development';
-
-
-
+export const NODE_ENV2 = process.env.NODE_ENV || 'development';
 
 // Paystack billing
-export const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_LIVE_SECRET_KEY || '';
+export const PAYSTACK_SECRET_KEY = NODE_ENV2 === 'production' ?  process.env.PAYSTACK_LIVE_SECRET_KEY || process.env.PAYSTACK_TEST_SECRET_KEY : '';
 export const TRIAL_PERIOD_DAYS = 7;
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
-
 
 export const ENABLE_LOGSTASH = false
