d33a4f02f2d8af8846b61ca78aa217a788fcbba5
mfadhili <mfadhili@github.com>
2025-07-15 03:29:24 +0300

    config testbed

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,7 +9,7 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const NODE_ENV2 = process.env.NODE_ENV || 'development';
 
 // Paystack billing
-export const PAYSTACK_SECRET_KEY = NODE_ENV === 'production' ?  process.env.PAYSTACK_LIVE_SECRET_KEY || process.env.PAYSTACK_TEST_SECRET_KEY : '';
+export const PAYSTACK_SECRET_KEY = NODE_ENV === 'production' ?  process.env.PAYSTACK_LIVE_SECRET_KEY : process.env.PAYSTACK_TEST_SECRET_KEY || '';
 export const TRIAL_PERIOD_DAYS = 7;
 
 if (!MONGODB_URI) {
